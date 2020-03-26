841f316c0903756ab8876107e50da80de1df0772
stephen bilham <stephenbilham@yahoo.com>
2020-03-26 13:27:19 -0700

    attach recipe to id and trying to render the ingredients to page

---
+++ starter/src/js/models/Recipe.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+export default class Recipe {
+  constructor(id) {
+    this.id = id;
+  }
+  async getRecipe() {
+    try {
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+      );
+      console.log(res);
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
+    } catch (error) {
+      console.log(error);
+      alert("something went wrong!");
+    }
+  }
+  calcTime() {
+    // Assuming that we need 15 minutes for each 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
+  }
+
+  parseIngredients() {
+    const unitsLong = [
+      "tablespoons",
+      "tablespoon",
+      "ounces",
+      "ounce",
+      "teaspoons",
+      "teaspoon",
+      "cups",
+      "pounds"
+    ];
+    const unitsShort = [
+      "tbsp",
+      "tbsp",
+      "oz",
+      "oz",
+      "tsp",
+      "tsp",
+      "cup",
+      "pound"
+    ];
+
+    const newIngredients = this.ingredients.map(el => {
+      //uniform units
+      let ingredient = el.toLowerCase();
+      unitsLong.forEach((unit, i) => {
+        ingredient = ingredient.replace(unit, unitsShort[i]);
+      });
+      //remove parantheses
+      ingredient = ingredient.replace(/ *\([^)]*\) */g, " ");
+      // 3) parse ingredients into count, unit and ingredients
+      return ingredient;
+    });
+    this.ingredients = newIngredients;
+  }
+}
